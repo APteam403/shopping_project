8814599b2a3eda985c71e456e2b33bbfb019e418
Amirreza Moghimi <Amir06mgh@gmail.com>
2025-08-02 22:12:41 -1200

    feat: Implement initial serializers for all models

--- Store/serializers.py
+++ Store/serializers.py
@@ -1,14 +1,49 @@
 from rest_framework import serializers
 from .models import *
 
-class ProductSerializrer(serializers.ModelSerializer):
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = '__all__'
+
+class SkinTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkinType
+        fields = '__all__'
+
+class IntegrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Integration
+        fields = '__all__'
+
+class ConcernSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Concerns
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-    product_id = serializers.IntegerField(read_only=True)
+    product_id = serializers.UUIDField(read_only=True)
     rating = serializers.FloatField(read_only=True)
-    skin_type = serializers.PrimaryKeyRelatedField(many=True, queryset=SkinType.objects.all())
-    concerns_targeted = serializers.PrimaryKeyRelatedField(many=True, queryset=Concerns.objects.all())
-    integrations = serializers.PrimaryKeyRelatedField(many=True, queryset=Integration.objects.all())
-    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tags.objects.all())
+
+    brand = serializers.SlugRelatedField(slug_field='name', queryset=Brand.objects.all())
+    category = serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all())
+
+    skin_type = serializers.SlugRelatedField(many=True, slug_field='name', queryset=SkinType.objects.all())
+    concerns_targeted = serializers.SlugRelatedField(many=True,slug_field='name', queryset=Concerns.objects.all())
+    integrations = serializers.SlugRelatedField( many=True, slug_field='name', queryset=Integration.objects.all())
+    tags = serializers.SlugRelatedField(many=True, slug_field='name', queryset=Tags.objects.all())
+    
