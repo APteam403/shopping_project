18e60a398d4df5609fb29e392db0566beee9526e
Amirreza Moghimi <Amir06mgh@gmail.com>
2025-08-02 22:13:16 -1200

    feat: Add all models endpoint in urls

--- Store/urls.py
+++ Store/urls.py
@@ -3,6 +3,12 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('products', views.ProductView)
+router.register('products', views.ProductViewSet)
+router.register('brands', views.BrandViewSet)
+router.register('categories', views.CategoryViewSet)
+router.register('tags', views.TagViewSet)
+router.register('concerns', views.ConcernViewSet)
+router.register('skinTypes', views.SkinTypeViewSet)
+router.register('integrations', views.IntegrationViewSet)
 
 urlpatterns = router.urls
