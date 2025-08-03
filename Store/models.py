871419921276b55f8368cb704066476d88868719
Amirreza Moghimi <Amir06mgh@gmail.com>
2025-08-02 22:11:56 -1200

    refactor: Add __str__ method

--- Store/models.py
+++ Store/models.py
@@ -5,31 +5,51 @@ from uuid import uuid4
 class Brand(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
 class SkinType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
 class Concerns(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
 class Integration(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Tags(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
-    product_id = models.UUIDField(primary_key=True, default=uuid4)
+    product_id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
+
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
     skin_type = models.ManyToManyField(SkinType)
     concerns_targeted = models.ManyToManyField(Concerns)
     integrations = models.ManyToManyField(Integration)
-    price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)])
+    tags = models.ManyToManyField(Tags, related_name='products')
+
+    price = models.DecimalField(max_digits=15, decimal_places=2, validators=[MinValueValidator(0.01)])
     rating = models.FloatField( validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0.0 )
     image_urls = models.URLField(unique=True)
-    tags = models.ManyToManyField(Tags)
+
+    def __str__(self):
+        return self.name
