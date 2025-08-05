fd3bde5217e41754e5764bfd4836739c6d9d0a78
mohammadreza <m.r.hassani8585@gmail.com>
2025-08-06 00:16:11 +0330

    feat(Users): Create Models.py for Users app

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Users_info(models.Model):
+    User_Id = models.UUIDField(max_length=300, verbose_name='User-Id')
+    name = models.CharField(max_length=300, verbose_name='user-name')
+    email = models.EmailField(verbose_name='user-email')
+    device_type = models.TextField(max_length=300, verbose_name='mobile/desktop')
+    concerns = models.JSONField(default=list)
+    preferences = models.JSONField(default=list)
+    skin_type = models.IntegerChoices("skin_type", "dry oily combination sensitive")
