f7edc8f9f59f4e1d0172aa307270d8e2cac06bbf
mohammadreza <m.r.hassani8585@gmail.com>
2025-08-06 04:31:45 +0330

    feat(Users): Fix viesw.py and urls.py and sign.html

--- Users/views.py
+++ Users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 def contact_page(request):
     if request.method == 'POST':
-        return redirect('signin.html')
+        return redirect(reverse('signin_page'))
     return render(request, 'Users/sign.html')
 
-def signin_page(response):
-    return render(response, 'Users/signin.html')
-# Create your views here.
+def signin_page(request):
+    return render(request, 'Users/signin.html')
